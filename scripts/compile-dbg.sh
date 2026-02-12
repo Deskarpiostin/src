08eb9898c321e4d2dddf4cb46fa95ccca812cf66
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-02-12 16:23:53 +0200

    Update scripts

--- scripts/compile-dbg.sh
+++ scripts/compile-dbg.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 ./waf configure -T debug --use-ccache --64bits --prefix=../game --disable-warns #--sanitize=address,undefined
 ./waf build install -p -vv -j$(nproc)
--- scripts/compile-rel.sh
+++ scripts/compile-rel.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 ./waf configure -T release --use-ccache --64bits --prefix=../game --disable-warns
 ./waf build install -p -vv -j$(nproc)
