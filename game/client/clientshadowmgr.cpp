187e496a4a3ea5c78909e21d97ead28e46ec503c
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-02-14 11:58:30 +0200

    client: fix compile error

--- game/client/clientshadowmgr.cpp
+++ game/client/clientshadowmgr.cpp
@@ -2239,7 +2239,7 @@ inline ShadowType_t CClientShadowMgr::GetActualShadowCastType( IClientRenderable
 class CShadowLeafEnum : public ISpatialLeafEnumerator
 {
 public:
-	bool EnumerateLeaf( int leaf, int context )
+	bool EnumerateLeaf( int leaf, intp context )
 	{
 		m_LeafList.AddToTail( leaf );
 		return true;
