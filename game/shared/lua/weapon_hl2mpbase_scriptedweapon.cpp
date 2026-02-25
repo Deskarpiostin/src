1b92a5186952b30bd12f7c274b5d51f040558eac
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-02-25 17:16:42 +0200

    client: fix compile error again

--- game/shared/lua/weapon_hl2mpbase_scriptedweapon.cpp
+++ game/shared/lua/weapon_hl2mpbase_scriptedweapon.cpp
@@ -26,7 +26,7 @@
 #ifdef CLIENT_DLL
 #include <vgui_controls/Panel.h>
 #include <vgui/ISurface.h>
-#include "hud.h"
+#include "hudelement.h"
 #include "iclientmode.h"
 #include "hud_macros.h"
 #endif
