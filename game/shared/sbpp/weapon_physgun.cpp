530c53889000cfc97e595ad8a6390147efda6d8a
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-03-17 07:24:28 +0200

    game: shared: make physgun_rotation_speed convar userinfo

--- game/shared/sbpp/weapon_physgun.cpp
+++ game/shared/sbpp/weapon_physgun.cpp
@@ -93,9 +93,9 @@ ConVar physgun_light( "physgun_light", "0", FCVAR_REPLICATED );
 ConVar physgun_vm_glow( "physgun_vm_glow", "1", FCVAR_USERINFO | FCVAR_ARCHIVE );
 
 #ifdef ANDROID
-ConVar physgun_rotation_speed( "physgun_rotation_speed", "0.25", FCVAR_ARCHIVE, "physgun rotation speed" );
+ConVar physgun_rotation_speed( "physgun_rotation_speed", "0.25", FCVAR_USERINFO | FCVAR_ARCHIVE, "physgun rotation speed" );
 #else
-ConVar physgun_rotation_speed( "physgun_rotation_speed", "0.05", FCVAR_ARCHIVE, "physgun rotation speed" );
+ConVar physgun_rotation_speed( "physgun_rotation_speed", "0.05", FCVAR_USERINFO | FCVAR_ARCHIVE, "physgun rotation speed" );
 #endif
 
 static IPhysicsObject *GetPhysObjFromPhysicsBone( CBaseEntity *pEntity, short physicsbone )
