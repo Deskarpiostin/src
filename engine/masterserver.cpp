2af27c7ff66574504a88bca63804b90dfaa02eb7
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-03-19 16:22:10 +0200

    engine: update masterserver list

--- engine/masterserver.cpp
+++ engine/masterserver.cpp
@@ -31,10 +31,9 @@ extern ConVar sv_lan;
 static char g_MasterServers[][64] =
 {
 	"185.192.97.130:27010",
-#ifndef MOON
 	"168.138.92.21:27016",
 	"135.125.188.162:27010"
-#else
+#ifdef MOON
 	"ms.workbench.network:27010",
 	"oreo922.cn:27010"
 #endif
